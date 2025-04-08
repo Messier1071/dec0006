dd7f92177c1e1b24271762c81b6b554ca11b0ffc
Mess <45535471+Messier1071@users.noreply.github.com>
2025-04-08 12:41:01 -0300

    yer

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ianma/Área de Trabalho/Estrutura de Dados/dec0006
-# Build directory: C:/Users/ianma/Área de Trabalho/Estrutura de Dados/dec0006/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
